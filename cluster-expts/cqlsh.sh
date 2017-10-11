8dc93bab827de3c0bcc59c3c60b791362572c936
Max Prettyjohns <heshoots9999@gmail.com>
2017-10-11 17:46:26 +0100

    Add cluster files

--- cluster-expts/cqlsh.sh
+++ cluster-expts/cqlsh.sh
@@ -1,1 +1,1 @@
-docker run -it --link cassandra:cassandra cassandra cqlsh cassandra
+docker run -it --network clusterexpts_default --link clusterexpts_cassandra_1:cassandra cassandra cqlsh cassandra
---
+++ cluster-expts/submit/app.py
@@ -0,0 +1,5 @@
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql._
+
+sc.parallelize([1,2,3,4,5]).count()
+
